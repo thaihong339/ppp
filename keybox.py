a702b759cf897d67d50d28288fd4b73d03b29110
thaihong339 <thaivuhong09@gmail.com>
2025-07-18 23:28:04 +0700

    Update keybox.py

--- keybox.py
+++ keybox.py
@@ -30,10 +30,7 @@ token_cycle = itertools.cycle(tokens)
 
 search_queries = [
     "<AndroidAttestation>",
-    "<Keybox>",
-    "<StrongIntegrity>",
-    "<AndroidKeybox>",
-    "<ValidKeybox>"
+    "</CertificateChain>"
 ]
 
 save = Path(__file__).resolve().parent / "found_keybox"
